272a5b11199de06cae8f1e9625159855e4da08c6
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-18 18:40:46 -0500

    Update solution from NeetCode

--- products-of-array-discluding-self.py
+++ products-of-array-discluding-self.py
@@ -1,10 +1,9 @@
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        prod = [0] * n  # Initialize with correct size
-        rev_prod = [0] * n  # Initialize with correct size
-        res = [0] * n  # Initialize with correct size
-        for i in range(len(nums)):
-            if i == 0:
-                prod[i] = nums[i]
+                res[i] = rev_prod[1]
+            elif i == len(nums)-1:
+                res[i] = prod[i-1]
             else:
+                res[i] = prod[i-1]*rev_prod[i+1]
+        
+        return res
+
+        
