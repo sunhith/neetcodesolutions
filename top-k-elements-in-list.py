b1486d8d9841b35b1095eab5e63f96dce4ddb3a3
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-17 21:39:21 -0500

    Update solution from NeetCode

--- top-k-elements-in-list.py
+++ top-k-elements-in-list.py
@@ -2,9 +2,11 @@ class Solution:
 
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         dic = {}
+        # get the frequency of list
         for num in nums:
             dic[num] = 1 + dic.get(num , 0)
-        sorted_dic_values = sorted(dic.items(),key= lambda item:item[1], reverse = True)
+        # sort the dictionary based on the frequency in descending order
+        sorted_dic_values = sorted(dic.items(),key= lambda item:item[1], reverse = True) 
 
         res = []
         for item in sorted_dic_values:
