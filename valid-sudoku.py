7d61ecf8160fd01acad41484cb771640a3140e16
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-19 16:05:44 -0500

    Update solution for valid-sudoku

--- valid-sudoku.py
+++ valid-sudoku.py
@@ -1,8 +1,8 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row_hash = {}
-        col_hash = {}
-        threeByThreeHash = {}
+        row_hash = {} # checks for row has same number
+        col_hash = {} # checks for column has same number
+        threeByThreeHash = {} #store a tuple () each 3 by 3 matrix is treated as (row, col) eg: first 3 by 3 matrix as (0,0) - checks for matrix has same number
         for row in range(9):
             for col in range(9):
                 if board[row][col] in row_hash.get(row, set()) or board[row][col] in col_hash.get(col,set()) or board[row][col] in threeByThreeHash.get((row//3,col//3), set()):
