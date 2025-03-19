3beb10f8b293dc7c135eabd006986ae9e5edc632
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-19 15:53:31 -0500

    Update solution for valid-sudoku

--- valid-sudoku.py
+++ valid-sudoku.py
@@ -1,14 +1,15 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row_hash = set()
-        col_hash = set()
+        row_hash = {}
+        col_hash = {}
         threeByThreeHash = {}
         for row in range(9):
             for col in range(9):
-                if board[row][col] in row_hash and board[row][col] in col_hash and board[row][col] in threeByThreeHash.get((int(row/3),int(col/3)), set()):
+                if board[row][col] in row_hash.get(row, set()) or board[row][col] in col_hash.get(col,set()) or board[row][col] in threeByThreeHash.get((row//3,col//3), set()):
                     return False
-                row_hash.add(board[row][col])
-                col_hash.add(board[row][col])
-                threeByThreeHash.get((int(row/3),int(col/3)),set()).add(board[row][col])
+                if board[row][col] != '.':
+                    row_hash.setdefault(row, set()).add(board[row][col])
+                    col_hash.setdefault(col, set()).add(board[row][col])
+                    threeByThreeHash.setdefault((row//3,col//3), set()).add(board[row][col])
         return True
         
