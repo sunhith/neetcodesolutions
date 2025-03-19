c1b7ce5dab31450a6b634a81fa78661592c8f052
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-19 10:33:11 -0500

    Update solution for products-of-array-discluding-self

--- products-of-array-discluding-self.py
+++ products-of-array-discluding-self.py
@@ -1,28 +1,29 @@
-class Solution:
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        prod = [0] * n  # Initialize with correct size
+        rev_prod = [0] * n  # Initialize with correct size
+        res = [0] * n  # Initialize with correct size
+        for i in range(len(nums)):
+            if i == 0:
+                prod[i] = nums[i]
+            else:
+                prod[i] = prod[i-1] * nums[i]
+        
+        for i in range(len(nums)-1,-1, -1):
+            if i == len(nums)-1:
+                rev_prod[i] = nums[i]
+            else:
+                rev_prod[i] = rev_prod[i+1]*nums[i]
+        
+        for i in range(len(nums)):
+            if i == 0:
+                res[i] = rev_prod[1]
+            elif i == len(nums)-1:
+                res[i] = prod[i-1]
+            else:
+                res[i] = prod[i-1]*rev_prod[i+1]
+        # test
+        return res
 
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        prod = [0] * n  # Initialize with correct size
-        rev_prod = [0] * n  # Initialize with correct size
-        res = [0] * n  # Initialize with correct size
-        for i in range(len(nums)):
-            if i == 0:
-                prod[i] = nums[i]
-            else:
-                prod[i] = prod[i-1] * nums[i]
-
-        for i in range(len(nums)-1,-1, -1):
-            if i == len(nums)-1:
-                rev_prod[i] = nums[i]
-            else:
-                rev_prod[i] = rev_prod[i+1]*nums[i]
-
-        for i in range(len(nums)):
-            if i == 0:
-                res[i] = rev_prod[1]
-            elif i == len(nums)-1:
-                res[i] = prod[i-1]
-            else:
-                res[i] = prod[i-1]*rev_prod[i+1]
-        # test
-        return res
+        
