1aa3f8284cb63b269ff0a04a4a8e111f74882190
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-18 23:11:25 -0500

    Update solution from NeetCode

--- products-of-array-discluding-self.py
+++ products-of-array-discluding-self.py
@@ -1,22 +1,25 @@
-        for i in range(len(nums)-1,-1, -1):
-            if i == len(nums)-1:
-                prod[i] = prod[i-1] * nums[i]
-        
-                prod[i] = nums[i]
-            else:
-        for i in range(len(nums)):
-            if i == 0:
-        rev_prod = [0] * n  # Initialize with correct size
-        res = [0] * n  # Initialize with correct size
+class Solution:
+
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
         prod = [0] * n  # Initialize with correct size
-class Solution:
-    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        rev_prod = [0] * n  # Initialize with correct size
+        res = [0] * n  # Initialize with correct size
+        for i in range(len(nums)):
+            if i == 0:
+                prod[i] = nums[i]
+            else:
+                prod[i] = prod[i-1] * nums[i]
 
+        for i in range(len(nums)-1,-1, -1):
+            if i == len(nums)-1:
                 rev_prod[i] = nums[i]
+            else:
                 rev_prod[i] = rev_prod[i+1]*nums[i]
+
+        for i in range(len(nums)):
+            if i == 0:
                 res[i] = rev_prod[1]
             elif i == len(nums)-1:
                 res[i] = prod[i-1]
-                res[i] = prod[i-1]*rev_prod[i+1]
-        return res
+            else:
