aadb99bf7a76b0209452a5faa1596f7ce4646372
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-19 20:55:09 -0500

    Add solution for two-integer-sum-ii

---
+++ two-integer-sum-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = 1
+        sum = numbers[i]+numbers[j]
+        while sum < target:
+            j+=1
+            sum = numbers[i]+numbers[j]
+        if sum == target:
+            return [numbers[i],numbers[j]]
+        while sum == target:
+            if numbers[i]+numbers[j] > target:
+                j-=1
+            else:
+                i+=1
+            sum = numbers[i]+numbers[j]
+        return [numbers[i],numbers[j]]
+
+        
