1f79e27253890b7abe28c1ab954b6416915099a8
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-20 19:20:00 -0500

    Update solution for trapping-rain-water

--- trapping-rain-water.py
+++ trapping-rain-water.py
@@ -1,5 +1,9 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        # Solution-1 
+        # Tc - o(n)
+        # Sc - o(n)
+        """
         n = len(height)
         left_max = [0]*(n)
         right_max = [0]*(n)
@@ -16,4 +20,23 @@ class Solution:
             min_val = min(left_max[i], right_max[i]) - height[i]
             storage+=min_val
         return storage
+"""
+        # Solution-2 -> Two Pointer Approach 
+        # Tc - o(n)
+        # Sc - o(1)
+        n = len(height)
+        i,j = 0,n-1
+        lmax= height[0]
+        rmax= height[n-1]
+        res = 0
+        while(i<j):
+            if lmax < rmax:
+                res+= lmax-height[i]
+                i+=1
+                lmax = max(lmax, height[i])
+            else:
+                res+= rmax-height[j]
+                j-=1
+                rmax = max(rmax, height[j])
+        return res
 
