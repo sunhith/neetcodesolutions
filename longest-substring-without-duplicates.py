b3c2bd430d13b73c7cf18967efc4a49282ec37b4
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-03-22 00:54:24 -0500

    Update solution for longest-substring-without-duplicates

--- longest-substring-without-duplicates.py
+++ longest-substring-without-duplicates.py
@@ -1,5 +1,11 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
+        """
+        TC- O(n)- We traverse the string once using the two-pointer technique.
+        SC- O(m)- m is the number of unique characters in the substring
+        It uses a dictionary to store the last seen index of characters and adjusts the left 
+        pointer (i) whenever a duplicate is found with an index + 1.
+        """
         n = len(s)
         if n == 0:
             return 0
@@ -11,11 +17,24 @@ class Solution:
             if s[j] in dic and dic[s[j]]>=i:
                 i = dic[s[j]]+1
             dic[s[j]] = j
-            print(dic)
             j+=1
             count = j-i
             res = max(res,count)
         return res
+
+        """
+        Same Logic as above code
+        mp = {}
+        l = 0
+        res = 0
+        
+        for r in range(len(s)):
+            if s[r] in mp:
+                l = max(mp[s[r]] + 1, l)
+            mp[s[r]] = r
+            res = max(res, r - l + 1)
+        return res
+        """
 
             
 
