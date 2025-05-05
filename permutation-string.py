b508a1575ff73759d51ddfdd7ad9dc92755911e1
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-05-04 20:43:45 -0500

    Add solution for permutation-string

---
+++ permutation-string.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        freq = defaultdict(int)
+        res = 0
+        for i in range(len(s1)):
+            freq[s1[i]] = 1 + freq[s1[i]]
+        l,r = 0,0
+        while r < len(s2):
+            if s2[r] in freq:
+                if freq[s2[r]] > 0:
+                    freq[s2[r]] = freq[s2[r]] - 1
+                    r+=1
+                else:
+                    freq[s2[l]] = freq[s2[l]]+1
+                    l+=1
+            else:
+                l+=1
+                r+=1
+            print(l)
+            print(r)
+            if len(s1) == r-l:
+                return True
+        return False
+        
