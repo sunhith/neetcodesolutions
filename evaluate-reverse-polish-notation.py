45ae96a241116cad1efd46373c55748e28bc6000
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-05-05 17:59:24 -0500

    Update solution for evaluate-reverse-polish-notation

--- evaluate-reverse-polish-notation.py
+++ evaluate-reverse-polish-notation.py
@@ -4,17 +4,25 @@ class Solution:
             case "+": return val1 + val2
             case "-": return val1 - val2
             case "*": return val1 * val2
-            case "/": return val1 // val2
+            case "/": return int(val1 / val2)
+    
+    def is_int(self, s):
+        try:
+            int(s)
+            return True
+        except Exception as error:
+            return False
 
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         for i in range(len(tokens)):
-            if tokens[i].isdigit():
+            if self.is_int(tokens[i]):
                 stack.append(int(tokens[i]))
             else:
                 val2 = stack.pop()
                 val1 = stack.pop()
                 stack.append(self.opp(val1, val2, tokens[i]))
+                print(stack)
         return stack[0]
 
         
