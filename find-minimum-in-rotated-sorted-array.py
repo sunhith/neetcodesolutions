9ed28dd5dab06520f33f9264d45109338c9e282d
Sai Sunhith Reddy Nagireddy <55741107+sunhith@users.noreply.github.com>
2025-05-11 23:14:52 -0500

    Update solution for find-minimum-in-rotated-sorted-array

--- find-minimum-in-rotated-sorted-array.py
+++ find-minimum-in-rotated-sorted-array.py
@@ -4,6 +4,9 @@ class Solution:
         l, r = 0, len(nums) - 1
 
         while l <= r:
+            if nums[l] < nums[r]:
+                res = min(res, nums[l])
+                break
             m = (l + r) // 2
             res = min(res, nums[m])
             if nums[m] >= nums[l]:
